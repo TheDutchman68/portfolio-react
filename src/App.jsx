c2bb85636fe225880a77f03a3a8a8595a352570a
TheDutchman68 <nd527589@gmail.com>
2026-01-28 15:16:28 +0200

    Add React Router and Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
 
 function App() {
 
   return (
-    <>
-    <body>
-      <p>Hello</p>
-    </body>
-    </>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+        </Routes>
+      </BrowserRouter>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return(
+        <div className="app">
+            <h1>Hi, I'm Natanael 👋</h1>
+            <p>Frontend Developer specialized in React</p>
+        </div>
+    );
+}
+
+export default Home;
